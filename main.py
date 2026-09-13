627c142048cb879186bf2ef09f88df67c30e98e4
Atharva Negi <atharvanegi232@gmail.com>
2026-09-13 00:12:38 -0400

    chain and prompt template along with local model and lansmith integration

--- main.py
+++ main.py
@@ -1,15 +1,51 @@
 from dotenv import load_dotenv
-
-import os
+from langchain.chat_models import init_chat_model
+from langchain_core.prompts import PromptTemplate
 
 load_dotenv()
 
-
-
+from langchain_ollama import ChatOllama
 
 
 def main():
     print("Hello from langchain-course!")
+
+    information = """
+    Virat Kohli (born 5 November 1988) is an Indian international cricketer
+    and former all-format captain of the Indian national cricket team.
+    """
+
+    summary_template = """
+    Given the information {information} about a person, I want you to create:
+    1. A short summary
+    2. Two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"],
+        template=summary_template
+    )
+
+    model = init_chat_model(
+        "auto",
+        model_provider="openrouter",
+        temperature=0
+    )
+
+    # model = init_chat_model(
+    #     "gemma3:270m",
+    #     model_provider="ollama"
+    # )
+
+    chain = summary_prompt_template | model
+
+    response = chain.invoke({
+        "information": information
+    })
+
+    print(response)
+    print(response.content)
+
 
 if __name__ == "__main__":
     main()
